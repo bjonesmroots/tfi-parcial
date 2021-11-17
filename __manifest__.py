fa77f0d4ff6206e066494545a217ab1c9848be24
Brian <bjones@mroots.com.ar>
2021-11-16 19:24:52 -0500

    Reporte Ficha Inmobiliaria

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'reports/ficha_inmobiliaria.xml',
         'views/views.xml',
         'views/front_templates.xml',
         'data/planes.xml',
